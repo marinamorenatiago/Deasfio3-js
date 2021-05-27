ca8f4c8780a09509994b08e5e6f0c500a911e77d
marinamorenatiago <morenatiago@gmail.com>
2021-05-27 20:36:31 -0300

    Subindo o desafio 3 js

---
+++ main.js
@@ -0,0 +1,21 @@
+for (let x = 1; x <=10; x++){
+    console.log(x)
+}
+
+let y = 10;
+while(y >= 1) {
+    console.log(y)
+    y--
+}
+
+let z = 1;
+while(z <= 100) {
+    console.log(z)
+    z+=2
+}
+
+let q = 0;
+while(q <= 100) {
+    console.log(q)
+    q+=2
+}
